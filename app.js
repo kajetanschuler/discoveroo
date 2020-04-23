6a936c28793127839cae348612ff6ec172005ac7
Kajetan Schuler <kajetan.schuler@gmail.com>
2020-04-23 13:50:09 +0200

    Initial commit for sls

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-// Created - 02.04.2020 - by Kajetan
-
 const serverless = require('serverless-http');
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
 
-const app = express();
-
-// parse requests of content type: application/json
-app.use(bodyParser.json());
-
-// parse requests of conten type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded());
-
-// simple route
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to TravelAPI"});
-});
-
-// Include the routes
-require("./app/routes/city_routes.js")(app);
-require("./app/routes/country_routes.js")(app);
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
 module.exports.handler = serverless(app);
--- app/config/db_config.js
+++
@@ -1,8 +0,0 @@
-// Created - 02.04.2020 - by Kajetan
-
-module.exports = {
-    HOST: "travelapidatabase.chuesgmxloap.eu-central-1.rds.amazonaws.com",
-    USER: "TravelApiMaster",
-    PASSWORD: "TravelApi2020",
-    DB: "TravelApiDB",
-  };
