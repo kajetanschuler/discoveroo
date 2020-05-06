4f4baef131fbd2ead6514168bc0e4a91f5ae6596
Svenja Lang <lang.svenja@gmail.com>
2020-05-06 18:38:28 +0200

    added region route

---
+++ app/routes/region_routes.js
@@ -0,0 +1,13 @@
+module.exports = app => {
+    const region = require('../../../../travelapi_expressjs/app/controller/region_controller.js');
+
+    // Find all regions 
+    app.get("/api/v1/regions", region.findAllRegions);
+
+    //Find details for one region by regionCode or regionName
+    app.get("/api/v1/regions/name/:regionName", region.findRegionDetails);
+
+    //Find all cities in region by regionCode or regionName
+    app.get("/api/v1/regions/:region/:cities", region.findCitiesInRegion);
+
+}
