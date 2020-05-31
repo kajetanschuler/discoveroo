449311a1850aba256189e1bf6ec0379ba648fe50
Svenja Lang <lang.svenja@gmail.com>
2020-05-31 20:32:37 +0200

    funktioniert für alle Kombinationen von country und city Parameter

--- app/models/recommendation_model.js
+++ app/models/recommendation_model.js
@@ -21,7 +21,7 @@ const Recommendation = function() {
 // Function that returns recommendation according to preferences 
 function getCityParameters (req) {
 
-  if (typeof recomCountry != undefined){
+  if (recomCountry != undefined){
     recomCity.where=null
     recomCity.values=null
     cityParameters.length = 0;
@@ -85,7 +85,7 @@ function getCityParameters (req) {
 // Function that returns recommendation according to preferences 
 function getCountryParameters (req) {
 
-  if (typeof recomCountry != undefined){
+  if (recomCountry != undefined){
     recomCountry.where=null
     recomCountry.values=null
     countryParameters.length = 0;
@@ -121,40 +121,51 @@ function getCountryParameters (req) {
 
 Recommendation.getRecommendation =  (req, result) => {
   var cityParameters = getCityParameters(req);
-  var countryParameters = getCountryParameters(req);
-  var citySqlQuery = 'SELECT * FROM city_data INNER JOIN country_data ON city_data.countryCode = country_data.countryCode WHERE cityId IN (SELECT cityId FROM city_data WHERE ' + cityParameters.where
-  var countrySqlQuery = ' AND countryCode IN (SELECT countryCode FROM country_data WHERE ' + countryParameters.where + '))'
-  var cityInserts = cityParameters.values; var countryInserts = countryParameters.values;
-  var citySqlStatement= mysql.format(citySqlQuery, cityInserts); var countrySqlStatement = mysql.format(countrySqlQuery, countryInserts)
-  var sqlStatement = citySqlStatement + countrySqlStatement
-  console.log('countryStatement 1: ' + countryParameters.where)
-  sql.query(sqlStatement, (err, res)  => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return
-    }
+  var countryParameters = getCountryParameters(req)
+  if (countryParameters.values.length > 0) {
+    console.log('test')
+    var citySqlQuery = 'SELECT * FROM city_data INNER JOIN country_data ON city_data.countryCode = country_data.countryCode WHERE cityId IN (SELECT cityId FROM city_data WHERE ' + cityParameters.where
+    var countrySqlQuery = ' AND countryCode IN (SELECT countryCode FROM country_data WHERE ' + countryParameters.where + '))'
+    var cityInserts = cityParameters.values; var countryInserts = countryParameters.values;
+    var citySqlStatement= mysql.format(citySqlQuery, cityInserts); var countrySqlStatement = mysql.format(countrySqlQuery, countryInserts)
+    var sqlStatement = citySqlStatement + countrySqlStatement
+    console.log('countryStatement 1: ' + countryParameters.where)
+    sql.query(sqlStatement, (err, res)  => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return
+      }
 
-    if (res.length) {
-      result(err, res);
-      //obwohl length hier auf Null gesetzt wird, ist Array in getCityParameters und getCountryParameters NICHT leer --> length in Funktion auf null setzten
-      //cityValues.length = 0;
-      //cityParameters.length = 0;
-      //countryValues.length = 0;
-      //countryParameters.length = 0;
+      if (res.length) {
+        result(err, res);
+        console.log('countrySqlStatement 2: ' + countryParameters.where)
+        return;
+      }
+      
+      result({kind: "not_found"}, null);
 
-      //countryParameters.where =null; 
-      //cityParameters.values=null; 
-      //cityParameters.where=null; 
-      //countryParameters.where=null;
-      console.log('countrySqlStatement 2: ' + countryParameters.where)
-      //getCountryParameters(null)
-      return;
-    }
-    
-    result({kind: "not_found"}, null);
+    })
+  }
+  else {
+    var citySqlQuery = 'SELECT * FROM city_data INNER JOIN country_data ON city_data.countryCode = country_data.countryCode WHERE ' + cityParameters.where
+    var cityInserts = cityParameters.values; var countryInserts = countryParameters.values;
+    var citySqlStatement= mysql.format(citySqlQuery, cityInserts); 
+    sql.query(citySqlStatement, (err, res)  => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return
+      }
 
-  }); 
+      if (res.length) {
+        result(err, res);
+        return;
+      }
+      
+      result({kind: "not_found"}, null);
+    })
+  }
 };
 
 
