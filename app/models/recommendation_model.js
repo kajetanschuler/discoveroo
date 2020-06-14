a85c4b72758a3a4641082121bbb35b6ad7a7ed11
Svenja Lang <lang.svenja@gmail.com>
2020-06-14 19:30:08 +0200

    Problembehebung: temp=0 führt nicht zu 404 error + kleine "Schönheitsreperaturen"

--- app/models/recommendation_model.js
+++ app/models/recommendation_model.js
@@ -252,14 +252,13 @@ function getCountryParameters (req) {
 
 function getWeatherParameters (req){
   
-  recomWeather.where=0; 
-  recomWeather.values=0; 
+  recomWeather.where=null 
+  recomWeather.values=null 
   weatherParameters.length=0;
   weatherValues.length=0;
-  var tempMax; var tempMin; 
   var monthStart=0; monthEnd=0;
 
-  if (req.query.temp !== undefined && req.query.start !== undefined) {
+  if (req.query.temp !== undefined && req.query.start !== undefined && req.query.temp !== '0') {
     dateStart = req.query.start
     monthStart = dateStart.substr(5,2);
     if (req.query.end !== undefined) {
